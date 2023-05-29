ba58294af423dd3aa6406ec89432754854094374
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-05-29 16:43:27 +0600

    create uer service

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application } from 'express'
 import cors from 'cors'
+import userRoute from './app/modules/users/users.route'
 
 const app: Application = express()
 
+//using cors
 app.use(cors())
 
+//body parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//application route
+app.use('/api/v1/users', userRoute)
 
 export default app
