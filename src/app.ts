bd4fff10a85c9ceb8802d83ba1d4053833cdf216
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-05-29 12:57:23 +0600

    checking list-staged

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express, { Application } from "express";
-import cors from "cors";
+import express, { Application } from 'express'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-export default app;
+export default app
