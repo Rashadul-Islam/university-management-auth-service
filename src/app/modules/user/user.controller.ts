8600639447c7ff8952956161ba8c7c696a2b2f9b
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-06-05 12:59:27 +0600

    [DONE] ZOD validation

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,19 @@
-import { RequestHandler } from 'express'
-import { UserService } from './user.service'
+import { RequestHandler } from 'express';
+import { UserService } from './user.service';
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
-    const result = await UserService.createUser(req.body.user)
+    const result = await UserService.createUser(req.body.user);
     res.status(200).json({
       success: true,
       message: 'user created successfully!',
       data: result,
-    })
+    });
   } catch (err) {
-    next(err)
+    next(err);
   }
-}
+};
 
 export const UserController = {
   createUser,
-}
+};
