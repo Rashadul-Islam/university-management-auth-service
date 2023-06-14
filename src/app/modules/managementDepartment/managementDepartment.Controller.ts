b061d1d41522ad928a907996fbcd50488f37105f
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-06-14 17:29:42 +0600

    [Done] Management Department

---
+++ src/app/modules/managementDepartment/managementDepartment.Controller.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import { paginationFields } from '../../../constants/pagination';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { IManagementDepartment } from './managementDepartment.interface';
+import { managementDepartmentFilterableFields } from './managementDepartment.constant';
+import { ManagementDepartmentService } from './managementDepartment.service';
+
+const createManagementDepartment = catchAsync(
+  async (req: Request, res: Response) => {
+    const { ...ManagementDepartmentData } = req.body;
+    const result = await ManagementDepartmentService.createManagementDepartment(
+      ManagementDepartmentData
+    );
+    sendResponse<IManagementDepartment>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Management department created successfully',
+      data: result,
+    });
+  }
+);
+
+const getAllManagementDepartments = catchAsync(
+  async (req: Request, res: Response) => {
+    const filters = pick(req.query, managementDepartmentFilterableFields);
+    const paginationOptions = pick(req.query, paginationFields);
+
+    const result =
+      await ManagementDepartmentService.getAllManagementDepartments(
+        filters,
+        paginationOptions
+      );
+
+    sendResponse<IManagementDepartment[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Management departments retrieved successfully',
+      meta: result.meta,
+      data: result.data,
+    });
+  }
+);
+
+const getSingleManagementDepartment = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result =
+      await ManagementDepartmentService.getSingleManagementDepartment(id);
+
+    sendResponse<IManagementDepartment>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Management department fetched successfully',
+      data: result,
+    });
+  }
+);
+
+const updateManagementDepartment = catchAsync(
+  catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const updatedData = req.body;
+    const result = await ManagementDepartmentService.updateManagementDepartment(
+      id,
+      updatedData
+    );
+
+    sendResponse<IManagementDepartment>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Management department updated successfully',
+      data: result,
+    });
+  })
+);
+
+const deleteManagementDepartment = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await ManagementDepartmentService.deleteByIdFromDB(id);
+
+    sendResponse<IManagementDepartment>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Management department deleted successfully',
+      data: result,
+    });
+  }
+);
+
+export const ManagementDepartmentController = {
+  createManagementDepartment,
+  getAllManagementDepartments,
+  getSingleManagementDepartment,
+  updateManagementDepartment,
+  deleteManagementDepartment,
+};
---
+++ src/app/modules/managementDepartment/managementDepartment.route.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { ManagementDepartmentValidation } from './managementDepartment.validation';
+import { ManagementDepartmentController } from './managementDepartment.Controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-management',
+  validateRequest(
+    ManagementDepartmentValidation.createManagementDepartmentZodSchema
+  ),
+  ManagementDepartmentController.createManagementDepartment
+);
+
+router.get(
+  '/:id',
+  ManagementDepartmentController.getSingleManagementDepartment
+);
+
+router.patch(
+  '/:id',
+  validateRequest(
+    ManagementDepartmentValidation.updateManagementDepartmentZodSchema
+  ),
+  ManagementDepartmentController.updateManagementDepartment
+);
+
+router.delete(
+  '/:id',
+  ManagementDepartmentController.deleteManagementDepartment
+);
+
+router.get('/', ManagementDepartmentController.getAllManagementDepartments);
+
+export const ManagementDepartmentRoutes = router;
